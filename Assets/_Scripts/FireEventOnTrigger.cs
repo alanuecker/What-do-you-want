8fa63343e9e195df7f33cd5617b0f9f62668d355
Tobias Bruch <tobias.bruch@gmx.de>
2017-04-01 20:58:26 +0200

    Add follower models

---
+++ Assets/_Scripts/FireEventOnTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class FireEventOnTrigger : MonoBehaviour {
+
+    [SerializeField] public UnityEvent _onTriggerEnter;
+
+	void OnTriggerEnter(){
+		_onTriggerEnter.Invoke();
+	}
+}
